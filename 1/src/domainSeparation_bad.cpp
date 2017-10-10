9017902dbe05985ff0fc9426461887536bf28a35
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-10 12:29:34 -0600

    Create domainSeparation_bad.cpp

---
+++ 1/src/domainSeparation_bad.cpp
@@ -0,0 +1,21 @@
+// domainSeparation_bad.cpp
+
+// This is a bad example of domain separation.
+
+// Chris Bugg
+// COSC 4010
+// 10/10/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Domain Separation Bad" << endl;
+
+	int howdy;
+
+	cin >> howdy;
+
+	return 0;
+}
