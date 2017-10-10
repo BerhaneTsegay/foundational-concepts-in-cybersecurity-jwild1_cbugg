bc32d8be7e7c6eb42bafec521be4d62197646833
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-10 13:13:16 -0600

    Create modularity_good.cpp

---
+++ 5/modularity_good.cpp
@@ -0,0 +1,21 @@
+// modularity_good.cpp
+
+// This is a good example of domain separation.
+
+// Chris Bugg
+// COSC 4010
+// 10/10/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "modularity" << endl;
+
+	int howdy;
+
+	cin >> howdy;
+
+	return 0;
+}
