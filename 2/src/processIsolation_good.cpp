4fe9f42432dc6846c77246be2a80cc39bf3659be
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-10 13:07:49 -0600

    Create processIsolation_good.cpp

---
+++ 2/src/processIsolation_good.cpp
@@ -0,0 +1,21 @@
+// processIsolation_good.cpp
+
+// This is a good example of domain separation.
+
+// Chris Bugg
+// COSC 4010
+// 10/10/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Process Isolation" << endl;
+
+	int howdy;
+
+	cin >> howdy;
+
+	return 0;
+}
