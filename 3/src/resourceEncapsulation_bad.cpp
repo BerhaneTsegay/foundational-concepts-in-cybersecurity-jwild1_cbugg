7149a54812b2a8ebc69dbf9e6465a4ebd33741db
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-11 22:42:05 -0600

    Update resourceEncapsulation_bad.cpp

--- 3/src/resourceEncapsulation_bad.cpp
+++ 3/src/resourceEncapsulation_bad.cpp
@@ -1,6 +1,9 @@
 // resourceEncapsulation_bad.cpp
 
-// This is a good example of domain separation.
+// This is a bad example of resource encapsulation. 
+//	It uses c++'s public/private declarations to allow 
+//	other functions (main) access to private variables
+//	other than by dedicated get/set-type methods.
 
 // Chris Bugg
 // COSC 4010
@@ -9,13 +12,41 @@
 #include <iostream>
 using namespace std;
 
+class secret
+{
+
+public:
+
+	int secret_number = 42;	// Defining a secret number not to be shared with other functions/classes	
+	
+	int get_secret()	// A dedicated function for getting the value of 'secret_number'
+	{
+
+		int lies = 1;	// Secure obfuscation seeding
+
+		lies = lies + secret_number;	// Obfuscating the real secret so we never share it
+
+		return lies;	// Returning only what we want to the calling class/function
+	}
+};
+
 int main()
 {
-	cout << "Resource Encapsulation" << endl;
+	cout << "Resource Encapsulation - Good" << endl;
 
-	int howdy;
+	secret secret_keeper;	// Defining our secret class
 
-	cin >> howdy;
+	int what_is_it;
+
+	what_is_it = secret_keeper.get_secret();	// Getting the 'secret_number' through the defined methods
+
+	cout << "get_secret() says: " << what_is_it << endl;
+
+	what_is_it = secret_keeper.secret_number;	// Getting the 'secret_number' directly from the class
+
+	cout << "secret_number says: " << what_is_it << endl;
+	
+	getchar();
 
 	return 0;
 }
