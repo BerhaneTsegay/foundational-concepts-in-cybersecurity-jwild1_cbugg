933ba815ca277ed79454a6391eb5055b5073eadc
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-12 12:18:47 -0600

    Update modularity_bad.cpp

--- 5/src/modularity_bad.cpp
+++ 5/src/modularity_bad.cpp
@@ -1,21 +1,31 @@
-// modularity_bad.cpp
-
-// This is a good example of domain separation.
-
-// Chris Bugg
-// COSC 4010
-// 10/10/17
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	cout << "modularity" << endl;
+	int ans = 0;
+	// You have to set the size of the array and declare elements. The numbers indicates which student
+	// the floats are their grade the character is their letter grade and the string prints there name.
+	int numbers[10] = {1,2,3,4,5,6,7,8,9,0};
 
-	int howdy;
+	float floats[10] = {90.1,80.2,70.3,60.4,50.5,40.6,30.7,20.8,10.9,0.0};
 
-	cin >> howdy;
+	char chracters[10] = {'A','B','C','D','F','F','F','F','F','F'};
 
-	return 0;
+	string strings[10] = {"1 has 90.1 which is an A","2 has 80.2 which is an B","3 has 70.3 which is an C","4 has 60.4 which is an D","5 has 50.5 which is an F","6 has 40.6 which is an F","7 has 30.7 which is an F","8 has 20.8 which is an F","9 has 10.9 which is an F","0 has 0.0 which is an F"};
+	cout << " do you want to see the arrays non 0 for yes" << endl;
+	cin >> ans;
+	if (ans != 0)
+	{
+		cout << numbers << endl;
+		cout << floats << endl;
+		cout << chracters << endl;
+		cout << strings << endl;
+	}
+	cout << " 0 to exit." << endl;
+	cin >> ans;
+	if (ans == 0)
+	{
+		exit.0;
+	}
 }
