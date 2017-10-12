18b0f103feb3ade8cb7a402ab4a1cae5e9755347
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-11 21:43:09 -0600

    Update processIsolation_bad.cpp

--- 2/src/processIsolation_bad.cpp
+++ 2/src/processIsolation_bad.cpp
@@ -1,21 +1,63 @@
 // processIsolation_bad.cpp
 
-// This is a good example of domain separation.
+// This is a bad example of process isolation. 
+// 	It is a multi-threaded program which doesn't use a mutex lock to 
+//	control which process (technically thread) has access to write (stdout). 
+
+// Code shamelessly stolen and modded
+// Intended as a basic multithreaded program example
+// Originally 'Example 2' by user usmanmalik on the go4expert.com forum
+// https://www.go4expert.com/articles/writing-multithreaded-program-cpp-t29980/
+
+// More stolen code. Mutex example from:
+// http://www.cplusplus.com/reference/mutex/mutex/lock/
 
 // Chris Bugg
 // COSC 4010
 // 10/10/17
 
 #include <iostream>
-using namespace std;
+#include <thread>
+#include <mutex>
 
-int main()
+std::mutex mtx;
+
+static const int num_of_threads = 10;
+
+//This is the function that will run in parallel
+// It will be passed to the thread
+void thread_method(int thread_id) 
 {
-	cout << "Process Isolation" << endl;
+	
+	// mtx.lock();
+	
+	std::cout << "thread " << thread_id << std::endl;
 
-	int howdy;
+	// mtx.unlock();
+}
 
-	cin >> howdy;
+int main() 
+{
 
+	std::thread threads[num_of_threads];
+	//This statement will launch multiple threads in loop
+	for (int i = 0; i < num_of_threads; ++i) {
+		threads[i] = std::thread(thread_method, i);
+	}
+
+	// mtx.lock();
+
+	std::cout << "main\n";
+
+	// mtx.unlock();
+
+	//This is how we join the new thread with main
+	for (int i = 0; i < num_of_threads; ++i) {
+		threads[i].join();
+	}
+
+	std::cout << "done, hit a key ";
+
+	getchar();	// Takes a char from a user (aka pause-after done functionality)
 	return 0;
 }
