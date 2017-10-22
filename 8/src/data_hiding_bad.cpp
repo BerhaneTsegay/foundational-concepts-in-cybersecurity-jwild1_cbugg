fd25d8e252df8f0197d3c0d7e016253c73504bc1
cbugg-uwyo <31521667+cbugg-uwyo@users.noreply.github.com>
2017-10-22 11:16:11 -0600

    Create data_hiding_bad.cpp

---
+++ 8/src/data_hiding_bad.cpp
@@ -0,0 +1,21 @@
+// leastPrivilege_good.cpp
+
+// This is a good example of domain separation.
+
+// Chris Bugg
+// COSC 4010
+// 10/10/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Least Privilege" << endl;
+
+	int howdy;
+
+	cin >> howdy;
+
+	return 0;
+}
